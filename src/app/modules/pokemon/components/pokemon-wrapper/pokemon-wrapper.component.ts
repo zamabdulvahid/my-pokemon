452a3648b4d087c18ec1a141fd6713140b6e37eb
zamabdulvahid <zam.abdulvahid@gmail.com>
2022-03-14 21:05:51 +0000

    Added route wrapper component for pokemon module

---
+++ src/app/modules/pokemon/components/pokemon-wrapper/pokemon-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-wrapper',
+  templateUrl: './pokemon-wrapper.component.html',
+  styleUrls: ['./pokemon-wrapper.component.scss']
+})
+export class PokemonWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/pokemon/pokemon.module.ts
+++ src/app/modules/pokemon/pokemon.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PokemonRoutingModule } from './pokemon-routeing.module';
+import { PokemonWrapperComponent } from './components/pokemon-wrapper/pokemon-wrapper.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
-import { PokemonRoutingModule } from './pokemon-routeing.module';
 @NgModule({
   declarations: [
+    PokemonWrapperComponent,
     PokemonListComponent,
     PokemonDetailComponent
   ],

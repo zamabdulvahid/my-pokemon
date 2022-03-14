12cbf6812c71b39f9c46b5d7ef600e29beb29bca
zamabdulvahid <zam.abdulvahid@gmail.com>
2022-03-14 20:19:55 +0000

    Exported header & footer components

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -10,6 +10,10 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }

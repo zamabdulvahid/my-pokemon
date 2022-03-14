e2dd555243450c3d019069964afea8cd5c6963bb
zamabdulvahid <zam.abdulvahid@gmail.com>
2022-03-14 21:07:37 +0000

    Updated app routing with lazy load support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonModule } from './modules/pokemon/pokemon.module';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: '',
+  children: [
+    {
+      path: '',
+      redirectTo: '/pokemon/list',
+      pathMatch: 'full'
+    },
+    {
+      path: 'pokemon',
+      loadChildren: () => import('./modules/pokemon/pokemon.module').then(module => module.PokemonModule)
+    },
+    {
+      path: '**',
+      redirectTo: 'pokemon/list'
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

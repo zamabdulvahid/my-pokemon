89e8506c0e2d632beaece35673500a1c50c8c6ad
zamabdulvahid <zam.abdulvahid@gmail.com>
2022-03-15 00:57:43 +0000

    Added markup and api call to get list of pokemons

--- src/app/modules/pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/app/modules/pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { Router } from '@angular/router';
+import { RestService } from 'src/app/modules/core/services/rest.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  pokemons = [];
+  subscriptions: Subscription[] = [];
+
+  constructor(private http: RestService, private router: Router) { }
+
+  set subscription(subscription: Subscription) {
+    this.subscriptions.push(subscription);
+  }
 
   ngOnInit(): void {
+    this.getPokemons();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription ? subscription.unsubscribe() : 0);
+  }
+
+  getPokemons(): void {
+    this.subscription = this.http.getData('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0').subscribe(response => {
+      response.results.forEach((pokemon, index) => {
+        this.subscription = this.http.getData(pokemon.url).subscribe(result => {
+          response.results[index].data = result;
+        });
+      });
+      this.pokemons = response.results;
+      console.log('pokemons', this.pokemons);
+    });
+  }
+
+  showPokemonDetails(): void {
+    this.router.navigate(['./pokemon/detail']);
   }
 
 }
